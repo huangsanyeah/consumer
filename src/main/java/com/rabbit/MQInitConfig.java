66970c658175a34e0913aeb0d2fe038c0e615286
huangweiyue <huangweiyue@quyiyuan.com>
2018-06-19 18:49:56 +0800

    Initial commit

---
+++ src/main/java/com/rabbit/MQInitConfig.java
@@ -0,0 +1,35 @@
+package com.rabbit;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description
+ * @Author huangweiyue
+ * @Date Created in 2018-06-19 16:51
+ * @ModifiedBy
+ * @Version v1.0
+ */
+//@Configuration
+//@ConditionalOnProperty(prefix = "spring.rabbitmq",name = "enable", matchIfMissing = false)
+@Component
+public class MQInitConfig {
+    // 队列名称
+    final static String queueName="hello";
+
+    @Bean(name = "link")
+    public Queue queueMessage() {
+        return new Queue("insurance.link");
+    }
+
+    @Bean(name = "hb")
+    public Queue queueMessages() {
+        return new Queue("insurance.hb");
+    }
+
+//    @Bean
+//    public MessageConverter jsonMessageConverter() {
+//        return new JsonMessageConverter();
+//    }
+}
